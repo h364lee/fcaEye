170b550849c53216eef25de0a6d75b04d829543b
Caleb Lee <hoyoung0834@gmail.com>
2026-09-24 23:30:59 -0400

    image revision

--- stimObj/objectGen.py
+++ stimObj/objectGen.py
@@ -11,26 +11,26 @@ shapes = ["tri", "square"]
 for size in sizes:
     for colour in colours:
         for shape in shapes:
-            img = Image.new("RGBA", (400, 400), (0, 0, 0, 0))
+            img = Image.new("RGBA", (1200, 1200), (0, 0, 0, 0))
             draw = ImageDraw.Draw(img)
 
             fill = (0, 0, 0) if colour == "black" else (255, 255, 255)
             outline = (0, 0, 0) 
 
             if size == "big":
-                left, top, right, bottom = 50, 50, 350, 350
+                left, top, right, bottom = 150, 150, 1050, 1050
             else:
-                left, top, right, bottom = 150, 150, 250, 250
+                left, top, right, bottom = 450, 450, 750, 750
 
             if shape == "tri":
                 top_vertex = ((left + right) // 2, top)
                 bottom_left = (left, bottom)
                 bottom_right = (right, bottom)
-                draw.polygon([top_vertex, bottom_left, bottom_right], fill=fill, outline=outline, width=1)
+                draw.polygon([top_vertex, bottom_left, bottom_right], fill=fill, outline=outline, width=3)
             elif shape == "square":
-                draw.rectangle([left, top, right, bottom], fill=fill, outline=outline, width=1)
+                draw.rectangle([left, top, right, bottom], fill=fill, outline=outline, width=3)
 
             filename = f"{output_dir}/{size}_{colour}_{shape}.png"
             img.save(filename)
 
-            print('I have completed my job as an image generating machine, Caleb!')
+print('I have completed my job as an image generating machine, Caleb!')
